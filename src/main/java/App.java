fe52124d5e38de60cb13190f119504f22f906efa
olgatolk <olga.s.tolkacheva@yandex.ru>
2022-04-01 11:01:42 +0300

    Ура!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,22 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+
+        boolean helloWorld = bean == bean1;
+        boolean cat = beanCat == beanCat1;
+
+        System.out.println(helloWorld);
+        System.out.println(cat);
+
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(beanCat.getName());
+        System.out.println(beanCat1.getName());
     }
 }
